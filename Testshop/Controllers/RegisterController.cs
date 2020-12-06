dad36affd4412d73ae7d5e00a263a59757047444
Nichkamon Promputta <fainichkamon@gmail.com>
2020-12-07 00:16:38 +0700

    Add project files.

---
+++ Testshop/Controllers/RegisterController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Testshop.Models;
+using System.IO;
+
+namespace Testshop.Controllers
+{
+    public class RegisterController : Controller
+    {
+        TestShopEntities db = new TestShopEntities();
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult SaveData(User user)
+        {
+            user.IsValid = false;
+            db.Users.Add(user);
+            db.SaveChanges();
+            return Json("Register Successfull", JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult CheckValidUser(User user)
+        {
+            string result = "Fail";
+            var DataItem = db.Users.Where(x => x.UserEmail == user.UserEmail && x.UserPassword == user.UserPassword).SingleOrDefault();
+            if (DataItem != null)
+            {
+                Session["UserID"] = DataItem.IDuser.ToString();
+                Session["UserName"] = DataItem.UserName.ToString();
+                result = "Success";
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AfterLogin()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Testshop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Testshop.MvcApplication" Language="C#" %>
